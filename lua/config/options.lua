49d84143e40c042a0adada4995fa00d613d4c2ea
TNH <thomasnhprivat@gmail.com>
2025-07-20 13:39:27 +0200

    added hlsearch

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,10 +12,10 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+vim.opt.hlsearch = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -40,5 +40,3 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
-
-vim.opt.winborder = "rounded"
