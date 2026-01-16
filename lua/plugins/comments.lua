b8ab58e04bb4f9b4d07f4735b954ca60f352d994
TNH <thomasnhprivat@gmail.com>
2026-01-16 10:59:27 +0100

    changed the colors of comments, and icons

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -4,29 +4,24 @@ return {
     event = "BufReadPost",
     opts = {
         colors = {
-            error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-            warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-            info = { "DiagnosticInfo", "#2563EB" },
-            hint = { "DiagnosticHint", "#10B981" },
-            default = { "Identifier", "#7C3AED" },
-            test = { "Identifier", "#FF00FF" },
-
             -- Your Custom Soft Colors
-            soft_red = { "#f38ba8" },
-            soft_peach = { "#fab387" },
-            soft_green = { "#a6e3a1" },
-            soft_blue = { "#89b4fa" },
-            soft_purple = { "#cba6f7" },
+            soft_error = { "#f38ba8" },
+            soft_warning = { "#f9e2af" },
+            soft_hack = { "#fab387" },
+            soft_info = { "#89b4fa" },
+            soft_note = { "#a6e3a1" },
+            soft_perf = { "#cba6f7" },
+            soft_test = { "#f5c2e7" },
         },
 
         keywords = {
-            FIX = { icon = " ", color = "soft_red", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-            TODO = { icon = " ", color = "soft_blue" },
-            HACK = { icon = " ", color = "soft_peach" },
-            WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-            PERF = { icon = " ", color = "soft_purple", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-            NOTE = { icon = " ", color = "soft_green", alt = { "INFO" } },
-            TEST = { icon = "󰂈 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+            FIX = { icon = " ", color = "soft_error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+            TODO = { icon = " ", color = "soft_info" },
+            HACK = { icon = " ", color = "soft_hack" },
+            WARN = { icon = " ", color = "soft_warning", alt = { "WARNING", "XXX" } },
+            PERF = { icon = "󰓅 ", color = "soft_perf", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            NOTE = { icon = " ", color = "soft_note", alt = { "INFO" } },
+            TEST = { icon = "󰙨 ", color = "soft_test", alt = { "TESTING", "PASSED", "FAILED" } },
         },
     }
 }
