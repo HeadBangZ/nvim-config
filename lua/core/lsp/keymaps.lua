72827716d0f9597383a75712a121ebce729c6b87
TNH <private@me.tnhprivate.dk>
2026-04-09 18:04:33 +0200

    fixed the lsp keymaps

--- lua/core/lsp/keymaps.lua
+++ lua/core/lsp/keymaps.lua
@@ -1,23 +1,26 @@
+-- lua/core/lsp/keymaps.lua
 local M = {}
 
 M.on_attach = function(client, bufnr)
     local opts = { buffer = bufnr, noremap = true, silent = true }
 
-    vim.keymap.set("n", "gd", "<Cmd>FzfLua lsp_definitions()<CR>", opts)
-    vim.keymap.set("n", "gD", "<Cmd>FzfLua lsp_declarations()<CR>", opts)
-    vim.keymap.set("n", "grr", "<Cmd>FzfLua lsp_references()<CR>", opts)
-    vim.keymap.set("n", "gri", "<Cmd>FzfLua lsp_implementations()<CR>", opts)
-    vim.keymap.set("n", "grt", "<Cmd>FzfLua lsp_typedefs()<CR>", opts)
-    vim.keymap.set("n", "gs", "<Cmd>FzfLua lsp_document_symbols()<CR>", opts)
-    vim.keymap.set("n", "gS", "<Cmd>FzfLua lsp_workspace_symbols()<CR>", opts)
-    vim.keymap.set("n", "<leader>fd", "<Cmd>FzfLua diagnostics_workspace()<CR>", opts)
+    -- Use the correct Lua call syntax
+    vim.keymap.set("n", "gd", "<Cmd>lua require('fzf-lua').lsp_definitions()<CR>", opts)
+    vim.keymap.set("n", "gD", "<Cmd>lua require('fzf-lua').lsp_declarations()<CR>", opts)
+    vim.keymap.set("n", "grr", "<Cmd>lua require('fzf-lua').lsp_references()<CR>", opts)
+    vim.keymap.set("n", "gri", "<Cmd>lua require('fzf-lua').lsp_implementations()<CR>", opts)
+    vim.keymap.set("n", "grt", "<Cmd>lua require('fzf-lua').lsp_typedefs()<CR>", opts)
+    vim.keymap.set("n", "gs", "<Cmd>lua require('fzf-lua').lsp_document_symbols()<CR>", opts)
+    vim.keymap.set("n", "gS", "<Cmd>lua require('fzf-lua').lsp_workspace_symbols()<CR>", opts)
+    vim.keymap.set("n", "<leader>fd", "<Cmd>lua require('fzf-lua').diagnostics_workspace()<CR>", opts)
 
+    -- Native LSP functions
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<C-a>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-    -- Diagnostics (The modern, non-deprecated way)
+    -- Diagnostics
     vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
 end
