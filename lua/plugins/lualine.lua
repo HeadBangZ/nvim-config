23ee5499e1ad2cd5832974e5d7ba94825e573215
TNH <private@me.tnhprivate.dk>
2026-04-17 12:00:02 +0200

    removed lualine dependency from arrow

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,33 @@
 local status_lualine, lualine = pcall(require, "lualine")
 if not status_lualine then
-    return 
+    return
 end
 
-local arrow_status = {
-    function()
-        local success, statusline = pcall(require, "arrow.statusline")
-        if success then
-            return statusline.text_for_statusline_with_icons()
+local function marks_status()
+    local local_marks = vim.fn.getmarklist(vim.api.nvim_get_current_buf())
+    local global_marks = vim.fn.getmarklist()
+
+    local l_count = 0
+    local g_count = 0
+
+    for _, m in ipairs(local_marks) do
+        if m.mark:match("'[a-z]") then
+            l_count = l_count + 1
         end
-    end,
-    padding = { left = 1, right = 1 },
-}
+    end
+
+    for _, m in ipairs(global_marks) do
+        if m.mark:match("'[A-Z]") then
+            g_count = g_count + 1
+        end
+    end
+
+    if l_count == 0 and g_count == 0 then
+        return ""
+    end
+
+    return string.format("󰈚 %d 󰳊 %d", l_count, g_count)
+end
 
 lualine.setup({
     options = {
@@ -23,7 +39,10 @@ lualine.setup({
     sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff" },
-        lualine_c = { "filename", arrow_status },
+        lualine_c = {
+            "filename",
+            { marks_status, color = { fg = "#ffc799" } }
+        },
         lualine_x = { "diagnostics", "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
