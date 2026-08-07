a985dd0498662f202d902e1cdec9ab1c5a6f9457
TNH <private@me.tnhprivate.dk>
2026-08-07 15:29:54 +0200

    removed comments

--- lua/custom/terminal.lua
+++ lua/custom/terminal.lua
@@ -27,13 +27,6 @@ local function get_float_config()
     }
 end
 
--- local function setup_term_options()
---     vim.opt_local.number = false
---     vim.opt_local.relativenumber = false
---     vim.opt_local.signcolumn = "no"
---     vim.cmd("startinsert")
--- end
-
 local function get_shell()
     if vim.fn.has("win32") == 1 then
         if vim.fn.executable("pwsh") == 1 then
@@ -75,41 +68,8 @@ vim.keymap.set("t", "<leader>st", function()
     toggle_float()
 end, { desc = "Terminal: Toggle Float" })
 
--- Exit terminal insert mode so standard Vim navigation (Ctrl-U, Ctrl-D, k, j) works for scrolling
 vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], opts)
 
 return {
     toggle_float = toggle_float,
 }
-
--- local function toggle_split()
---     for _, win in ipairs(vim.api.nvim_list_wins()) do
---         if vim.api.nvim_win_get_buf(win) == state.buf then
---             vim.api.nvim_win_close(win, true)
---             return
---         end
---     end
---
---     local buf = get_or_create_buf()
---     vim.cmd("botright 30split")
---     vim.api.nvim_win_set_buf(0, buf)
---
---     if vim.bo[buf].buftype ~= "terminal" then
---         vim.cmd.term(get_shell())
---     end
---     setup_term_options()
--- end
---
--- local opts = { noremap = true, silent = true }
---
--- vim.keymap.set("n", "<leader>st", toggle_split, { desc = "Terminal: Toggle Split" })
--- vim.keymap.set("t", "<leader>st", function()
---     vim.cmd("stopinsert")
---     toggle_split()
--- end, { desc = "Terminal: Toggle Split" })
---
--- vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], opts)
---
--- return {
---     toggle_split = toggle_split
--- }
