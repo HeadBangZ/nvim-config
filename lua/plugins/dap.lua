3950631fb3c7d0c8ca40b69e7efd86332860faed
TNH <thomasnhprivat@gmail.com>
2026-08-25 14:18:19 +0200

    changed controls for dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,10 +11,10 @@ dap.listeners.before.event_terminated.dapui_config = function() dapui.close() en
 dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
 
 -- Keymaps
+vim.keymap.set("n", "<F1>", dap.step_over, { desc = "Debug: Step Over" })
+vim.keymap.set("n", "<F2>", dap.step_into, { desc = "Debug: Step Into" })
+vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
 vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
-vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
-vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
 vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle UI" })
 
