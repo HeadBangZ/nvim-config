1ffab7a9bedfe814a728f864f7fae0251de17325
TNH <thomasnhprivat@gmail.com>
2026-09-03 08:46:01 +0200

    updated plugins, and changed from oil.nvim to canola.nvim

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ vim.schedule(function()
         { src = "https://github.com/nvim-tree/nvim-web-devicons" },
         { src = "https://github.com/rafamadriz/friendly-snippets" },
         { src = "https://github.com/ibhagwan/fzf-lua" },
-        { src = "https://github.com/stevearc/oil.nvim" },
+        { src = "https://github.com/barrettruth/canola.nvim" },
         { src = "https://github.com/sindrets/diffview.nvim" },
         { src = "https://github.com/lewis6991/gitsigns.nvim" },
         -- { src = "https://github.com/tpope/vim-fugitive" },
